51746893d73b6f5c6357bcb43c09f71264a58f77
Esther Jenslin <esther@katenterprise.com>
2025-11-26 17:06:15 +0530

    api calls post,delete

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
+import 'dart:ffi';
 
+import 'package:basic_1/modal/post_modal.dart';
 import 'package:basic_1/service/api_service.dart';
 import 'package:flutter/material.dart';
 
@@ -28,10 +30,143 @@ class _HomePageState extends State<HomePage> {
     log('api call done:$post');
   }
 
+  final addformKey = GlobalKey<FormState>();
+  TextEditingController postIdController = TextEditingController();
+  TextEditingController titleControler = TextEditingController();
+  TextEditingController bodyController = TextEditingController();
+  void addItemUIAlert() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add Item'),
+          content: Container(
+            height: 200,
+
+            child: Form(
+              key: addformKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: postIdController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Id',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter id';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    controller: titleControler,
+                    decoration: InputDecoration(
+                      labelText: 'title',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter title';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    controller: bodyController,
+                    decoration: InputDecoration(
+                      labelText: 'body',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter body';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (addformKey.currentState!.validate()) {
+                  final parsedId = int.tryParse(postIdController.text);
+                  if (parsedId == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Id must be a number')),
+                    );
+                    return;
+                  }
+                  ApiService().createPost(
+                    Post(
+                      userId: 90,
+                      id: parsedId,
+                      title: titleControler.text,
+                      body: bodyController.text,
+                    ),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('post added was successfully!')),
+                  );
+                  postIdController.clear();
+                  titleControler..clear();
+                  bodyController..clear();
+                  Navigator.pop(context);
+                } else {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text('Not validated')));
+                }
+              },
+              child: Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addItemUIAlert();
+          /* ApiService()
+              .createPost(
+                Post(
+                  userId: 1,
+                  id: 101,
+                  title: 'New Post',
+                  body: 'This is a new post created via API',
+                ),
+              )
+              .then((createdPost) {
+                log('Created Post: ${createdPost.toJson()}');
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Post Created: ${createdPost.title}')),
+                );
+              })
+              .catchError((error) {
+                log('Error creating post: $error');
+              });*/
+        },
+        child: Icon(Icons.add),
+      ),
       body: isLoading
           ? Center(child: CircularProgressIndicator())
           : ListView.builder(
@@ -46,7 +181,21 @@ class _HomePageState extends State<HomePage> {
                   padding: EdgeInsets.all(16),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
+
                     children: [
+                      Align(
+                        alignment: AlignmentGeometry.centerRight,
+                        child: IconButton(
+                          onPressed: () {
+                            log('Delete Id:${post[index].id}');
+                            ApiService().deletePost(post[index].id);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Deleted Sucessfully!!')),
+                            );
+                          },
+                          icon: Icon(Icons.delete, color: Colors.red),
+                        ),
+                      ),
                       Text(
                         post[index].title,
                         style: TextStyle(
--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -2,11 +2,12 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:basic_1/modal/post_modal.dart';
-import 'package:http/http.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ApiService {
   Future<List<Post>> fetchPost() async {
-    final response = await get(
+    final response = await http.get(
       Uri.parse('https://jsonplaceholder.typicode.com/posts'),
       headers: {'Content-Type': 'application/json'},
     );
@@ -18,6 +19,47 @@ class ApiService {
       return jsonList.map((json) => Post.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load posts');
+    }
+  }
+
+  Future<Post> createPost(Post post) async {
+    final response = await http.post(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(post.toJson()),
+    );
+    log('Response body: ${response.body}');
+    log('Status code: ${response.statusCode}');
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return Post.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create post');
+    }
+  }
+
+  Future deletePost(int id) async {
+    final response = await http.delete(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts/$id'),
+      headers: {'Content-Type': 'application/json'},
+    );
+    log('response code:${response.statusCode}');
+    if (response.statusCode == 200) {
+      log('deleted successfully');
+    } else {
+      log('not deleted');
+    }
+  }
+
+  Future<dynamic> updatePost(int id, Map<String, dynamic> data) async {
+    final response = await http.put(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts/$id'),
+      headers: {'Content-Type': 'application/json'},
+    );
+    log('response code:${response.statusCode}');
+    if (response.statusCode == 200) {
+      log('updated successfully');
+    } else {
+      log('not updated');
     }
   }
 }
